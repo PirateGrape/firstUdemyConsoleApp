9644aa72c42971e2bbfaeb3cd104e7787d28beb6
Alexandr Vasilev <wassan66@gmail.com>
2021-01-07 16:25:08 +0700

    New loops for many questions  and review on how many movies are watched

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let numberOfFilms = 
-prompt("Сколько фильмов вы уже посмотрели?", '');
+'use strict';
+
+let numberOfFilms =
+    prompt("Сколько фильмов вы уже посмотрели?", '');
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -9,19 +11,55 @@ let personalMovieDB = {
     privat: false
 };
 
-let a =
-prompt("Один из последних просмотренных фильмов?", ''),
+//вариант с циклом for
+for (let i = 1; i <= numberOfFilms; i++) {
 
-b =
-prompt("На сколько оцените его?", ''),
+    let a = //название фильма
+    prompt("Один из последних просмотренных фильмов?", '');
 
-c =
-prompt("Один из последних просмотренных фильмов?", ''),
+    if(a == null) {
+        a = prompt('Пожалуйста, введите название одного из последних просмотренных фильмов', '');
+    } else if(a.length == 0) {
+        a = prompt("Пожалуйста, укажите название одного из последних просмотренных фильмов");
+    } else if(a.length > 50) {
+        a =prompt("Пожалуйста, введите название покороче (макс. 50 символов)");
+    }
+    let b = //оценка фильма
+    prompt("На сколько оцените его?", '');
 
-d =
-prompt("На сколько оцените его?", '');
+    personalMovieDB.movies = personalMovieDB.movies + " " + `${a} ${b}`;
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+//вариант с циклом while
+/*
+while(numberOfFilms > 0) {
+    let a =
+    prompt("Один из последних просмотренных фильмов?", '');
+    if(a.length == 0) {
+        a = prompt("Пожалуйста, укажите название одного из последних просмотренных фильмов");
+    }
+    else if(a.length > 50) {
+        a =
+        prompt("Пожалуйста, введите название покороче (макс. 50 символов)");
+    }
+    let b =
+    prompt("На сколько оцените его?", '');
+
+    personalMovieDB.movies = personalMovieDB.movies + " " + `${a} ${b}`;
+
+    numberOfFilms--;
+}
+*/
 
 console.log(personalMovieDB);
+
+//Проверка на количетсво просмотренных фильмов
+if(numberOfFilms < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (numberOfFilms > 10 && numberOfFilms < 30) {
+    console.log('Вы классический зритель');
+} else if (numberOfFilms > 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
